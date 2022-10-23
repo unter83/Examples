f12bcf12aa3946dd1fe81a91f868056d4fd87359
Unter <unter83@gmail.com>
2022-10-23 15:58:27 +0300

    Made first massive program

---
+++ Example009_NewArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int Max (int arg, int max)
+{
+    if (arg > max) max = arg;
+    return max;
+}
+
+int[] array = {1, 34, 53, 64, 677, 23, 85, 4003, 22};
+short i = 0;
+int max = array[0];
+while (i < 9)
+ {
+    max = Max(array[i], max);
+    i++;
+}
+Console.WriteLine(max);
