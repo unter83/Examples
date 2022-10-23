3c625c198852332f2e96c8ef3038b733bd9ab603
Unter <unter83@gmail.com>
2022-10-23 17:18:58 +0300

    Random Array program

---
+++ Example011_ArrayRandom/Program.cs
@@ -0,0 +1,48 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int position = 0;
+    while (position < col.Length)
+    {
+        Console.Write(col[position] + " ");
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int index = 0;
+    int position = -1;
+    while (index < collection.Length)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }    
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 4;
+array[6] = 4;
+PrintArray(array);
+
+Console.WriteLine();
+
+int pos = IndexOf(array, 44);
+Console.WriteLine(pos);
