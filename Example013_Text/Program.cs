d27f25a450b918262e99def3e3ba06053d26a731
Unter <unter83@gmail.com>
2022-10-26 15:57:58 +0300

    text progam

---
+++ Example013_Text/Program.cs
@@ -0,0 +1,19 @@
+﻿string text = Console.ReadLine();
+
+string Replace (string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int letter_count = text.Length;
+    for (int i = 0; i < letter_count; i++)
+    {
+        if (text[i] == oldValue)
+            result = result + $"{newValue}"; 
+         else
+           result = result  + $"{text[i]}"; 
+    }
+    return result;
+}
+
+string newText = Replace (text, ' ', '_');
+
+Console.WriteLine(newText);
